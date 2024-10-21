2dce5f811ccab3ff7cc34ae234dacb304a96ebc9
akcase18 <aidan.case@casefamily.us>
2024-10-21 12:37:10 -0600

    Modified cmake so we have 3 executables

---
+++ include/lab5.h
@@ -0,0 +1,7 @@
+#ifndef LAB5_H
+#define LAB5_H
+
+#define OUT_PIN 1
+#define DELAY_MS 10
+
+#endif
--- src/sleep.c
+++ src/sleep.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <pico/stdlib.h>
+#include "lab5.h"
 
 int main(void)
 {
